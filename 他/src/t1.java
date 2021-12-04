991d76c0156ca6e04c629dc45d611ecbbcdcfc77
1059393848 <1059393848@qq.com>
2021-12-04 16:03:20 +0800

    更改1

--- 他/src/t1.java
+++ 他/src/t1.java
@@ -1,2 +1,48 @@
+import java.awt.*;
+import java.security.PolicySpi;
+import java.util.Arrays;
+
 public class t1 {
+//    private static final Arrays;
+//    private static final PolicySpi;
+//    public static final int
+
+    public static void main(String[] args) {
+        System.out.println();
+        int a = 1;
+        System.out.println("t1.main");
+        List list = new List();
+
+
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("a = " + a);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("args = " + args);
+        }
+        for (String arg : args) {
+
+        }
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+
+        }
+        for (String arg : args) {
+
+        }
+        if (a == null) {
+
+        }
+        if (a != null) {
+
+        }
+        List b = null;
+
+        if (b == null) {
+
+        }
+        if (b != null) {
+
+        }
+
+    }
 }
